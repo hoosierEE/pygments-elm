f820713bdf659c3baf52594ed44238612e5ce2fe
hoosierEE <ashroyer@gmail.com>
2014-12-03 22:27:06 -0500

    starting with strings and keywords

--- elm.py
+++ elm.py
@@ -14,7 +14,7 @@ from pygments.token import Comment, Error, Keyword, Name, Number, Operator, Punc
 
 __all__ = ['ElmLexer']
 
-class JLexer(RegexLexer):
+class ElmLexer(RegexLexer):
     """
     For `Elm <http://elm-lang.org/>`_ source code.
     """
@@ -24,25 +24,20 @@ class JLexer(RegexLexer):
     filenames = ['*.elm']
     mimetypes = ['text/x-elm']
 
-    validName = r'\b[a-zA-Z]\w*'
+    validName = r'\b\w[\w\']*'
 
     tokens = {
         'root': [
 
             # Comments TODO: just started
             (r'--.*', Comment.Single),
-            (r'\n+\s*{-', Comment.Multiline, 'comment'),
-            (r'\s*Note.*', Comment.Single),
+            (r'\n+\s*{-(|)?', Comment.Multiline, 'comment'),
 
             # Whitespace
             (r'\s+', Text),
 
             # Strings
-            (r"'", String, 'singlequote'),
-
-            # Definitions
-            (r'0\s+:\s*0|noun\s+define\s*$', Name.Entity, 'nounDefinition'),
-            (r'\b(([1-4]|13)\s+:\s*0)|((adverb|conjunction|dyad|monad|verb)\s+define)\b', Name.Function, 'explicitDefinition'),
+            (r'"', String, 'doublequote'),
 
             # Flow Control
             (words(('for_', 'goto_', 'label_'), suffix=validName+'\.'), Name.Label),
@@ -58,24 +53,12 @@ class JLexer(RegexLexer):
 
             # Standard Library
             (words((
-                'ARGV', 'CR', 'CRLF', 'DEL', 'Debug',
-                'EAV', 'EMPTY', 'FF', 'JVERSION', 'LF',
-                'LF2', 'Note', 'TAB', 'alpha17', 'alpha27',
-                'apply', 'bind', 'boxopen', 'boxxopen', 'bx',
-                'clear', 'cutLF', 'cutopen', 'datatype', 'def',
-                'dfh', 'drop', 'each', 'echo', 'empty',
-                'erase', 'every', 'evtloop', 'exit', 'expand',
-                'fetch', 'file2url', 'fixdotdot', 'fliprgb', 'getargs',
-                'getenv', 'hfd', 'inv', 'inverse', 'iospath',
-                'isatty', 'isutf8', 'items', 'leaf', 'list',
-                'nameclass', 'namelist', 'namelist', 'names', 'nc',
-                'nl', 'on', 'pick', 'pick', 'rows',
-                'script', 'scriptd', 'sign', 'sminfo', 'smoutput',
-                'sort', 'split', 'stderr', 'stdin', 'stdout',
-                'table', 'take', 'timespacex', 'timex', 'tmoutput',
-                'toCRLF', 'toHOST', 'toJ', 'tolower', 'toupper',
-                'type', 'ucp', 'ucpcount', 'usleep', 'utf8',
-                'uucp',
+                'as', 'case', 'class', 'data', 'default',
+                'deriving', 'do', 'else', 'export', 'foreign',
+                'hiding', 'jsevent', 'if', 'import', 'in',
+                'infix', 'infixl', 'infixr', 'instance', 'let',
+                'module', 'newtype', 'of', 'then', 'type',
+                'where', '_',
                 )), Name.Function),
 
             # Copula
@@ -97,9 +80,8 @@ class JLexer(RegexLexer):
         ],
 
         'comment': [
-            (r'[^)]', Comment.Multiline),
-            (r'^\)', Comment.Multiline, '#pop'),
-            (r'[)]', Comment.Multiline),
+            (r'^(-})', Comment.Multiline),
+            (r'^-}', Comment.Multiline, '#pop'),
         ],
 
         'explicitDefinition': [
@@ -111,31 +93,163 @@ class JLexer(RegexLexer):
         ],
 
         'numbers': [
-            (r'\b_{1,2}\b', Number),
-            (r'_?\d+(\.\d+)?(\s*[ejr]\s*)_?\d+(\.?=\d+)?', Number),
             (r'_?\d+\.(?=\d+)', Number.Float),
-            (r'_?\d+x', Number.Integer.Long),
             (r'_?\d+', Number.Integer),
-        ],
-
-        'nounDefinition': [
-            (r'[^)]', String),
-            (r'^\)', Name.Label, '#pop'),
-            (r'[)]', String),
         ],
 
         'parentheses': [
             (r'\)', Punctuation, '#pop'),
-            #include('nounDefinition'),
-            include('explicitDefinition'),
             include('root'),
         ],
 
-        'singlequote': [
-            (r"[^']", String),
-            (r"''", String),
-            (r"'", String, '#pop'),
+        'doublequote': [
+            (r'[^"]', String),
+            (r'"', String, '#pop'),
         ],
     }
 
+### elm.vim syntax file ##
+
+# " Vim syntax file
+# " Language: Elm (http://elm-lang.org/)
+# " Maintainer: Alexander Noriega
+# " Latest Revision: 01 November 2013
+
+# if exists("b:current_syntax")
+#   finish
+# endif
+
+# " Keywords
+# as case class data default deriving do else export foreign
+# hiding jsevent if import in infix infixl infixr instance let
+# module newtype of then type where _
+
+# " Builtin operators
+# syn match elmBuiltinOp "\.\."
+# syn match elmBuiltinOp "|\{,2\}"
+# syn match elmBuiltinOp ":"
+# syn match elmBuiltinOp "::"
+# syn match elmBuiltinOp "="
+# syn match elmBuiltinOp "\\"
+# syn match elmBuiltinOp "\""
+# syn match elmBuiltinOp "->"
+# syn match elmBuiltinOp "<-"
+# syn match elmBuiltinOp "\u2192"
+# syn match elmBuiltinOp "\u03BB"
+# syn match elmBuiltinOp "\$"
+# syn match elmBuiltinOp "&&"
+# syn match elmBuiltinOp "+"
+# syn match elmBuiltinOp "++"
+# syn match elmBuiltinOp "-"
+# syn match elmBuiltinOp "\."
+# syn match elmBuiltinOp "/"
+# syn match elmBuiltinOp "/="
+# syn match elmBuiltinOp "<"
+# syn match elmBuiltinOp "<="
+# syn match elmBuiltinOp "=="
+# syn match elmBuiltinOp ">"
+# syn match elmBuiltinOp ">="
+# syn match elmBuiltinOp "\^"
+# syn match elmBuiltinOp "\*"
+# syn match elmBuiltinOp "<\~"
+# syn match elmBuiltinOp "\~"
+
+# " Builtin types
+# syn keyword elmBuiltinType Bool Char False Float GT Int Just LT Maybe Nothing String True
+# syn keyword elmBuiltinType Time Date Text Order Element List Signal Tuple Either
+
+# " Special names
+# syntax match specialName "^main "
+
+# " Libraries
+# syn keyword elmCoreLibrary List Time Collage Prelude Random Signal String Http Set Color Keyboard Text WebSocket Automaton Mouse Transform2D Dict Char Maybe Json Window Experimental JavaScript Basics Either Date Element Touch Input
+
+# " Library functions
+
+# " Automaton
+# syn keyword elmBuiltinFunction run step (>>>) (<<<) combine pure state hiddenState count average
+# " Basics
+# syn keyword elmBuiltinFunction radians degrees turns fromPolar toPolar (+) (-) (*) (/) div rem mod (^) cos sin tan acos asin atan atan2 sqrt abs logBase clamp pi e (<) (>) compare min max (&&) (||) xor not otherwise round truncate floor ceiling toFloat (.) (|>) (<|) id fst snd flip curry uncurry
+# " Char
+# syn keyword elmBuiltinFunction isUpper isLower isDigit isOctDigit isHexDigit toUpper toLower toLocaleUpper toLocaleLower toCode fromCode
+# " Color
+# syn keyword elmBuiltinFunction rgba rgb lightRed red darkRed lightOrange orange darkOrange lightYellow yellow darkYellow lightGreen green darkGreen lightBlue blue darkBlue lightPurple purple darkPurple lightBrown brown darkBrown black white lightGrey grey darkGrey lightGray gray darkGray lightCharcoal charcoal darkCharcoal grayscale greyscale complement hsva hsv linear radial
+# " Date
+# syn keyword elmBuiltinFunction read toTime year month day dayOfWeek hour minute second
+# " Dict
+# syn keyword elmBuiltinFunction empty min max lookup findWithDefault member rotateLeft rotateRight rotateLeftIfNeeded rotateRightIfNeeded color_flip color_flipIfNeeded ensureBlackRoot insert singleton isBBlack moreBlack lessBlack moreBlackTree lessBlackTree del rem bubble remove_max balance blackish balance_node blacken redden remove map foldl foldr union intersect diff keys values toList fromList
+# " Either
+# syn keyword elmBuiltinFunction either isLeft isRight lefts rights partition
+# " Graphics.Collage
+# syn keyword elmBuiltinFunction defaultLine solid dashed dotted form filled textured gradient outlined traced sprite toForm group groupTransform move moveX moveY scale rotate alpha collage path segment polygon rect square oval circle ngon
+# " Graphics.Element
+# syn keyword elmBuiltinFunction widthOf heightOf sizeOf width height size opacity color tag link image fittedImage croppedImage tiledImage container spacer flow above below beside layers absolute relative middle topLeft topRight bottomLeft bottomRight midLeft midRight midTop midBottom middleAt topLeftAt topRightAt bottomLeftAt bottomRightAt midLeftAt midRightAt midTopAt midBottomAt up down left right inward outward
+# " Graphics.Input
+# syn keyword elmBuiltinFunction buttons button customButtons customButton checkboxes checkbox hoverables hoverable fields emptyFieldState field password email dropDown stringDropDown
+# " Http
+# syn keyword elmBuiltinFunction request get post send sendGet
+# " JavaScript
+# syn keyword elmBuiltinFunction toList toInt toFloat toBool toString fromList fromInt fromFloat fromBool fromString fromElement toElement
+# " JavaScript.Experimental
+# syn keyword elmBuiltinFunction toRecord fromRecord
+# " Json
+# syn keyword elmBuiltinFunction toString toJSString fromString fromJSString fromJSObject toJSObject
+# " Keyboard
+# syn keyword elmBuiltinFunction directions arrows wasd isDown shift ctrl space enter keysDown lastPressed
+# " List
+# syn keyword elmBuiltinFunction (::) (++) head tail last isEmpty map foldl foldr foldl1 foldr1 scanl scanl1 filter length reverse all any and or concat concatMap sum product maximum minimum partition zip zipWith unzip join intersperse take drop repeat
+# " Maybe
+# syn keyword elmBuiltinFunction maybe isJust isNothing justs
+# " Mouse
+# syn keyword elmBuiltinFunction position isDown isClicked clicks
+# " Prelude
+# syn keyword elmBuiltinFunction show readInt readFloat
+# " Random
+# syn keyword elmBuiltinFunction range float floatList
+# " Set
+# syn keyword elmBuiltinFunction empty singleton insert remove member union intersect diff toList fromList foldl foldr map
+# " Signal
+# syn keyword elmBuiltinFunction constant lift lift2 lift3 lift4 lift5 lift6 lift7 lift8 foldp merge merges combine count countIf keepIf dropIf keepWhen dropWhen dropRepeats sampleOn (<~) (~)
+# " String
+# syn keyword elmBuiltinFunction isEmpty cons uncons append concat length map filter reverse foldl foldr split join repeat sub left right dropLeft dropRight pad padLeft padRight trim trimLeft trimRight words lines toUpper toLower any all contain[s] startsWith endsWith indexes indices toInt toFloat toList fromList
+# " Text
+# syn keyword elmBuiltinFunction toText typeface monospace header link height color bold italic overline underline strikeThrough justified centered righted text plainText markdown asText
+# " Time
+# syn keyword elmBuiltinFunction millisecond second minute hour inMilliseconds inSeconds inMinutes inHours fps fpsWhen every since timestamp delay
+# " Touch
+# syn keyword elmBuiltinFunction touches taps
+# " Transform2D
+# syn keyword elmBuiltinFunction identity matrix rotation translation scale scaleX scaleY multiply
+# " WebSocket
+# syn keyword elmBuiltinFunction connect
+# " Window
+# syn keyword elmBuiltinFunction dimensions width height
+
+# " Comments
+# syn match elmTodo "[tT][oO][dD][oO]\|FIXME\|XXX" contained
+# syn match elmLineComment "--.*" contains=elmTodo,@spell
+# syn region elmComment matchgroup=elmComment start="{-|\=" end="-}" contains=elmTodo,elmComment,@spell
+
+# " String literals
+# syn region elmString start="\"" skip="\\\"" end="\"" contains=elmStringEscape
+# syn match elmStringEscape "\\u[0-9a-fA-F]\{4}" contained
+# syn match elmStringEscape "\\[nrfvb\\\"]" contained
+
+# " Number literals
+# syn match elmNumber "\(\<\d\+\>\)"
+# syn match elmNumber "\(\<\d\+\.\d\+\>\)"
+
+# let b:current_syntax = "elm"
+
+# hi def link elmKeyword            Keyword
+# hi def link elmBuiltinOp          Special
+# hi def link elmBuiltinType        Type
+# hi def link elmBuiltinFunction    Function
+# hi def link elmCoreLibrary        Type
+# hi def link elmTodo               Todo
+# hi def link elmLineComment        Comment
+# hi def link elmComment            Comment
+# hi def link elmString             String
+# hi def link elmNumber             Number
+# hi def link specialName           Special
 
